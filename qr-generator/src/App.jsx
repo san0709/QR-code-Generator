86ecafa08ac28b0b28ab0b9f7b169241f77810ef
san0709 <sandhiya6797@gmail.com>
2025-11-10 00:44:22 +0530

    save

--- qr-generator/src/App.jsx
+++ qr-generator/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-
-
-function QrCodeGenerator() {
-
-  return (
-    <main>
-      <h2>QR CODE GENERATOR</h2>
-    </main>
-  )
-  
-}
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-      
-    </>
-  )
+    <div className=" container flex flex-col min-w-xl justify-center items-center">
+      <h2>QR CODE GENERATOR</h2>
+      <img
+        alt="QR-Code"
+        src="https://placehold.jp/3d4070/ffffff/150x150.png"
+      ></img>
+      <div>
+        <label htmlFor="data">Data for QR Code</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="data">Img Size (e.g., 150)</label>
+        <input type="text" />
+      </div>
+      <div>
+        <button className="cursor-pointer">Generate QR Code</button>
+        <button className="cursor-pointer">Dowload QR Code</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
