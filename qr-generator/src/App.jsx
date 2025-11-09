42f44d8745b8eec4ae161ee93ed2987c5b206195
san0709 <sandhiya6797@gmail.com>
2025-11-10 00:03:10 +0530

    first commit

---
+++ qr-generator/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function QrCodeGenerator() {
+
+  return (
+    <main>
+      <h2>QR CODE GENERATOR</h2>
+    </main>
+  )
+  
+}
+
+
+function App() {
+
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default App
