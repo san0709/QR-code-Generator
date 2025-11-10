e73e25bee46d6243c4bab3ac8fbf1411f5a512ae
san0709 <sandhiya6797@gmail.com>
2025-11-11 00:31:27 +0530

    save

--- qr-generator/src/App.jsx
+++ qr-generator/src/App.jsx
@@ -2,26 +2,60 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [img, setImg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [qrData, setqrData] = useState("");
+  const [qrSize, setqrSize] = useState("");
+  async function generateQr() {
+    setLoading(true);
+    try {
+      const url = ` https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${encodeURIComponent(
+        qrData
+      )}`;
+      setImg(url);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  function downloadQr() {
+    fetch(img).then((response) => response.blob()).then((blob) => {
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob)
+    })
+  }
   return (
     <div className="container">
       <h2>QR CODE GENERATOR</h2>
-      <img
-        alt="QR-Code"
-        src="https://placehold.jp/3d4070/ffffff/150x150.png"
-      ></img>
+      {loading && <p>Please Wait...</p>}
+      {img && <img alt="QR-Code" src={img}></img>}
       <div>
-        <label className="label" htmlFor="data">
-          Data for QR Code
-        </label>
-        <input type="text" />
-        <label className="label" htmlFor="data">
-          Img Size (e.g., 150)
-        </label>
-        <input type="text" />
+        <label className="label">Data for QR Code</label>
+        <input
+          type="text"
+          value={qrData}
+          onChange={(e) => setqrData(e.target.value)}
+        />
+        <label className="label">Img Size (e.g., 150)</label>
+        <input
+          type="text"
+          value={qrSize}
+          onChange={(e) => setqrSize(e.target.value)}
+        />
       </div>
-      <div>
-        <button className="cursor-pointer">Generate QR Code</button>
-        <button className="cursor-pointer">Dowload QR Code</button>
+      <div className="btn">
+        <button
+          className="cursor-pointer"
+          onClick={generateQr}
+          disabled={loading}
+        >
+          Generate QR Code
+        </button>
+        <button className="cursor-pointer" onClick={downloadQr}>
+          Dowload QR Code
+        </button>
       </div>
     </div>
   );
