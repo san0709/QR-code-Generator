fd6572779f313b4fde0b4f5e034273dbcaac22d7
san0709 <sandhiya6797@gmail.com>
2025-11-10 23:32:28 +0530

    save

--- qr-generator/src/App.jsx
+++ qr-generator/src/App.jsx
@@ -3,18 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=" container flex flex-col min-w-xl justify-center items-center">
+    <div className="container">
       <h2>QR CODE GENERATOR</h2>
       <img
         alt="QR-Code"
         src="https://placehold.jp/3d4070/ffffff/150x150.png"
       ></img>
       <div>
-        <label htmlFor="data">Data for QR Code</label>
+        <label className="label" htmlFor="data">
+          Data for QR Code
+        </label>
         <input type="text" />
-      </div>
-      <div>
-        <label htmlFor="data">Img Size (e.g., 150)</label>
+        <label className="label" htmlFor="data">
+          Img Size (e.g., 150)
+        </label>
         <input type="text" />
       </div>
       <div>
